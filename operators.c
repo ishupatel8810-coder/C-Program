3c820dce4fa4933f5aa5e96fdb0f1e2c3f0aad41
ishu_patel <ishupatel8810@gmail.com>
2026-08-19 20:09:27 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 3;
+
+    printf("Addition: %d\n", x + y);
+    printf("Subtraction: %d\n", x - y);
+    printf("Multiplication: %d\n", x * y);
+    printf("Division: %d\n", x / y);
+    printf("Modulus: %d\n", x % y);
+
+    return 0;
+}
