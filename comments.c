1ad3e3fd3f9e0edf6a6df1e6974024747864b009
ishu_patel <ishupatel8810@gmail.com>
2026-08-19 20:01:02 +0530

    Create comments.c

---
+++ comments.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // This is a single-line comment
+    printf("Hello World!");
+
+    /*
+      This is a multi-line comment
+      It will not be executed.
+    */
+
+    return 0;
+}
