038cdb430bf12ef7d05af8d80c349ce9bf1ea8d3
ishu_patel <ishupatel8810@gmail.com>
2026-08-19 20:06:13 +0530

    Create data_types.c

---
+++ data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int myNum = 5;
+    float myFloat = 5.99;
+    char myLetter = 'D';
+
+    printf("%d\n", myNum);
+    printf("%f\n", myFloat);
+    printf("%c\n", myLetter);
+
+    return 0;
+}
