97e3f36ba4c9ddc9516a88dc2c714e9a375e5385
ishu_patel <ishupatel8810@gmail.com>
2026-08-19 20:11:34 +0530

    Add comparison operators example in C

---
+++ comparison_operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    printf("%d\n", x == y);
+    printf("%d\n", x != y);
+    printf("%d\n", x > y);
+    printf("%d\n", x < y);
+    printf("%d\n", x >= y);
+    printf("%d\n", x <= y);
+
+    return 0;
+}
