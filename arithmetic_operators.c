51b8d1e18650cf816a789f151d49d9294edb6ee6
ishu_patel <ishupatel8810@gmail.com>
2026-08-19 20:10:08 +0530

    Implement basic arithmetic operations in C

---
+++ arithmetic_operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 3;
+
+    printf("%d\n", x + y);
+    printf("%d\n", x - y);
+    printf("%d\n", x * y);
+    printf("%d\n", x / y);
+    printf("%d\n", x % y);
+
+    return 0;
+}
